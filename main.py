d2afefd3f6b21423179331db70efcbe9cf32110b
Jacob <JacobS@CSedge.org>
2019-07-24 09:34:38 -0700

    API

--- main.py
+++ main.py
@@ -2,6 +2,10 @@
 import webapp2
 import jinja2
 import os
+# import pokepy
+
+
+
 
 # this initializes the jinja2 environment
 the_jinja_env = jinja2.Environment(
@@ -13,16 +17,19 @@ the_jinja_env = jinja2.Environment(
 class HomeHandler(webapp2.RequestHandler): #homepage "/"
     def get(self):
         home_template = the_jinja_env.get_template('templates/home.html') #pulls in "home.html" template
+        # client = pokepy.V2Client()
+        # print(client.get_pokemon(14))
         self.response.write(home_template.render()) #serves home.html template back to front-end
 
 class AboutHandler(webapp2.RequestHandler):
     def get(self):
         about_template = the_jinja_env.get_template('templates/about.html')
-        self.response.write(about_template.render())  
+        self.response.write(about_template.render())
 
 class ResultsHandler(webapp2.RequestHandler):
     def get(self):
         # below are the form results from the form on home.html
+
         results_Dict = {
           'name': self.request.get('user-first-name'), #stores form input named 'user-first-name' under key 'name' which is the same name as the placeholder on 'results.html'
           'feeling': self.request.get('user-feeling') #stores form input under 'user-feeling' under key 'feeling' which is the same name as the placeholder on 'results.html'
@@ -30,12 +37,17 @@ class ResultsHandler(webapp2.RequestHandler):
         results_template = the_jinja_env.get_template('templates/results.html')
         self.response.write(results_template.render(results_Dict)) #passes in results_Dict that will fill the placeholders on results.html
 
+class PokemonHandler(webapp2.RequestHandler):
+    def get(self):
+        pokemon_template = the_jinja_env.get_template('templates/pokemon.html')
+        self.response.write(pokemon_template.render())
 
 # the routes / app configuration section
 app = webapp2.WSGIApplication([
   ('/', HomeHandler),
   ('/about', AboutHandler),
   ('/results', ResultsHandler),
+  ('/pokemon', PokemonHandler)
   ], debug=True)
 
 
--- scripts/script.js
+++ scripts/script.js
@@ -1,9 +1,27 @@
 $( document ).ready(function() { //required for jQuery to work
 
 //all javascript and jquery code here
+function getRandomPokemonLeft() {
+  pokemons = $.get(`https://pokeapi.co/api/v2/pokemon/15`)
+//   i = 0;
+//     for (i = 0; i < pokemons.length; i++) {
+//       pokemons[i] + "<br>";
+// }
+//     print(pokemons[i]);
+  // console.log("hi")
+  // console.log(pokemons.response)
+  // };
+//
+// console.log("hello")
+//
+// function getRandomPokemonLeft() {
+//   let randomNumber = Math.floor(Math.random() * 800) + 1;
+//   $.get(`https://pokeapi.co/api/v2/pokemon/${randomNumber}`).then(function(pokeData){
+//     console.log("Left Pokemon Data: ", pokeData);
+//   });
+// }
 
-
-
+ getRandomPokemonLeft()
 
 
 
