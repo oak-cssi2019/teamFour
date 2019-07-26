479d79e498d5d749342215582983561ef6c346a9
blimon <35636559+blimon@users.noreply.github.com>
2019-07-26 15:47:34 -0700

    update

--- main.py
+++ main.py
@@ -2,6 +2,10 @@
 import webapp2
 import jinja2
 import os
+# import pokepy
+
+
+
 
 # this initializes the jinja2 environment
 the_jinja_env = jinja2.Environment(
@@ -13,6 +17,8 @@ the_jinja_env = jinja2.Environment(
 class HomeHandler(webapp2.RequestHandler): #homepage "/"
     def get(self):
         home_template = the_jinja_env.get_template('templates/home.html') #pulls in "home.html" template
+        # client = pokepy.V2Client()
+        # print(client.get_pokemon(14))
         self.response.write(home_template.render()) #serves home.html template back to front-end
 
 class AboutHandler(webapp2.RequestHandler):
@@ -23,10 +29,35 @@ class AboutHandler(webapp2.RequestHandler):
 class ResultsHandler(webapp2.RequestHandler):
     def get(self):
         # below are the form results from the form on home.html
+
         results_Dict = {
-          'name': self.request.get('user-first-name'), #stores form input named 'user-first-name' under key 'name' which is the same name as the placeholder on 'results.html'
-          'feeling': self.request.get('user-feeling') #stores form input under 'user-feeling' under key 'feeling' which is the same name as the placeholder on 'results.html'
+          'q1': self.request.get('q1'),
+          'q1correct': "Gen 1",
+          'q2': self.request.get('q2'),
+          'q2correct': "Black and White",
+          'q3': self.request.get('q3'),
+          'q3correct': "88",
+          'q4': self.request.get('q4'),
+          'q4correct': "Mew",
+          'q5': self.request.get('q5'),
+          'q5correct': "Bulbasaur",
+          'q6': self.request.get('q6'),
+          'q6correct': "Satoshi",
+          'q7': self.request.get('q7'),
+          'q7correct': "19",
+          'q8': self.request.get('q8'),
+          'q8correct': "Dialga",
+          'q9': self.request.get('q9'),
+          'q9correct': "Generation 2",
+          'q10': self.request.get('q10'),
+          'q10correct': "Max",
+          'q11': self.request.get('q11'),
+          'q11correct': "Dusknior"
+
+           #stores form input named 'user-first-name' under key 'name' which is the same name as the placeholder on 'results.html'
+          # 'feeling': self.request.get('user-feeling') #stores form input under 'user-feeling' under key 'feeling' which is the same name as the placeholder on 'results.html'
         }
+        print(results_Dict)
         results_template = the_jinja_env.get_template('templates/results.html')
         self.response.write(results_template.render(results_Dict)) #passes in results_Dict that will fill the placeholders on results.html
 
@@ -35,13 +66,17 @@ class QuizHandler(webapp2.RequestHandler):
         quiz_template = the_jinja_env.get_template('templates/quiz.html')
         self.response.write(quiz_template.render())
         # raw_input("Who is jesus christ")
-
+class PokemonHandler(webapp2.RequestHandler):
+    def get(self):
+        pokemon_template = the_jinja_env.get_template('templates/pokemon.html')
+        self.response.write(pokemon_template.render())
 # the routes / app configuration section
 app = webapp2.WSGIApplication([
-  ('/', HomeHandler),
+  ('/home', HomeHandler),
   ('/about', AboutHandler),
   ('/results', ResultsHandler),
-  ('/quiz',QuizHandler)
+  ('/quiz',QuizHandler),
+  ('/pokemon', PokemonHandler)
   ], debug=True)
 
 
